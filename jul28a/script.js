50f68b7d08eba10f5d77f3bf710c5a39c17f0cf6
Tim Wissen <timwissennetwork@gmail.com>
2023-07-28 15:05:04 +0200

    added gCalendar-Embet + still dead text areas

---
+++ jul28a/script.js
@@ -0,0 +1,5 @@
+var googleCalendarURL = "https://calendar.google.com/calendar/embed?src=1c76b10b8fc8a21fc2cfad40b3ea63b606cbfde919090cc25dc18db1df1ff841%40group.calendar.google.com&ctz=Europe%2FBerlin";
+
+var iframe = document.getElementById("calendar1");
+
+iframe.src = googleCalendarURL;
